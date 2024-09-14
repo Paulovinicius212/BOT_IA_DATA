fdb23ad2180f153f6f891458a15b0a6399c05af9
PAULO VINICIUS <121190664+Paulovinicius212@users.noreply.github.com>
2024-09-14 02:26:19 -0300

    ia api

---
+++ exemplo_groc.py
@@ -0,0 +1,30 @@
+import os
+
+from groq import Groq
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.environ.get("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"),
+)
+
+
+def minha_pergunta_pro_groq(pergunta: str):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": pergunta,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+pergunta = "Quanto eu vendi ontem?"
+resposta = minha_pergunta_pro_groq(pergunta)
+print(resposta)
